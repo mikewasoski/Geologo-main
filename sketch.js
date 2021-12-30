9a37a93b3962509471f242f07541ecf060284585
mikewasoski <88514330+mikewasoski@users.noreply.github.com>
2021-12-30 15:36:45 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,36 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
 
-var engine, world;
-var hammer;
-var iron,stone,rubber,
 
-function setup(){
-    var canvas = createCanvas(1200,600);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
-    plane = new Plane(600,height,1200,20)
-    iron = new Iron(300,350);
-    stone = new Stone(700,320,100,100);
-    rubber = new Rubber(900,450,70);
-    hammer = new Hammer(10,100);
 
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+
+	Engine.run(engine);
+  
 }
 
-function draw(){
-    background("lightBlue");
-    Engine.update(engine);
 
-
-    plane.display();
-    hammer.display();
-    stone.display();
-    rubber.display();
-    iron.display();
-    
+function draw() {
+  rectMode(CENTER);
+  background(230);
  
+
+  groundObject.display();
+  dustbinObj.display();
+
 }
+
